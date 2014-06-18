6ab9a93b8e2f1a01d19a4887759aede5a8580fdb
Frank Luan <franklsf95@gmail.com>
2014-06-18 14:35:51 -0700

    authToken automatically reloads

--- js/main.coffee
+++ js/main.coffee
@@ -3,6 +3,7 @@ $rb = $('#response-body')
 baseURL = 'https://api.sketch-off.com/api/v1'
 
 prettify = (str) ->
+    console.log str
     str = JSON.stringify JSON.parse(str), null, 4
     str = str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")
     str.replace /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, (match) ->
@@ -28,16 +29,19 @@ $('#panel-input').keyup (e) ->
     return false
 
 $('#btn-token').click ->
-    bootbox.prompt 'Enter a User ID: ', (id) ->
-        console.log 'requesting auth token for: ' + id
-        $.getJSON baseURL+'/backdoor/'+id, (data) ->
-            $('#auth-token').val data
+    bootbox.prompt 'Enter a User ID: ', requestToken
+
+requestToken = (id) ->
+    console.log 'requesting auth token for: ' + id
+    $.getJSON baseURL+'/backdoor/'+id, (data) ->
+        $('#auth-token').val data
 
 $('#btn-go').click ->
     request $('#request-line').val()
 
 $('#base-url').change ->
     baseURL = $(this).val() + '/api/v1'
+    requestToken(1)
 
 request = (text) ->
     console.log 'requesting: ' + text
@@ -46,10 +50,18 @@ request = (text) ->
         console.log 'Assuming GET'
         url = verb
         verb = 'GET'
-    url = baseURL + url + '?auth_token=' + $('#auth-token').val()
-    $.ajax url,
+    authToken = 'auth_token=' + $('#auth-token').val()
+    con = '?'
+    if url.indexOf('?') != -1
+        con = '&'
+    url = baseURL + url + con + authToken
+
+    data = $('#request-body').val()
+    console.log 'data: ' + data
+    $.ajax
+        url: url
         type: verb
-        crossDomain: true
+        data: JSON.parse data
         success: (data, status, xhr) ->
             processResponse xhr
         error: (xhr, status, err) ->
--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 (function() {
-  var $rb, baseURL, prettify, processResponse, request;
+  var $rb, baseURL, prettify, processResponse, request, requestToken;
 
   $rb = $('#response-body');
+
+  $(document).ajaxSend(function(e, xhr, s) {
+    console.log('-----------------------------------');
+    console.log(e);
+    return console.log(xhr);
+  });
 
   baseURL = 'https://api.sketch-off.com/api/v1';
 
   prettify = function(str) {
+    console.log(str);
     str = JSON.stringify(JSON.parse(str), null, 4);
     str = str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     return str.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function(match) {
@@ -42,24 +49,27 @@
   });
 
   $('#btn-token').click(function() {
-    return bootbox.prompt('Enter a User ID: ', function(id) {
-      console.log('requesting auth token for: ' + id);
-      return $.getJSON(baseURL + '/backdoor/' + id, function(data) {
-        return $('#auth-token').val(data);
-      });
-    });
+    return bootbox.prompt('Enter a User ID: ', requestToken);
   });
+
+  requestToken = function(id) {
+    console.log('requesting auth token for: ' + id);
+    return $.getJSON(baseURL + '/backdoor/' + id, function(data) {
+      return $('#auth-token').val(data);
+    });
+  };
 
   $('#btn-go').click(function() {
     return request($('#request-line').val());
   });
 
   $('#base-url').change(function() {
-    return baseURL = $(this).val() + '/api/v1';
+    baseURL = $(this).val() + '/api/v1';
+    return requestToken(1);
   });
 
   request = function(text) {
-    var url, verb, _ref;
+    var authToken, con, data, url, verb, _ref;
     console.log('requesting: ' + text);
     _ref = text.split(' '), verb = _ref[0], url = _ref[1];
     if (url == null) {
@@ -67,10 +77,18 @@
       url = verb;
       verb = 'GET';
     }
-    url = baseURL + url + '?auth_token=' + $('#auth-token').val();
-    return $.ajax(url, {
+    authToken = 'auth_token=' + $('#auth-token').val();
+    con = '?';
+    if (url.indexOf('?') !== -1) {
+      con = '&';
+    }
+    url = baseURL + url + con + authToken;
+    data = $('#request-body').val();
+    console.log('data: ' + data);
+    return $.ajax({
+      url: url,
       type: verb,
-      crossDomain: true,
+      data: JSON.parse(data),
       success: function(data, status, xhr) {
         return processResponse(xhr);
       },
